df2a123838a0afbd373ce9a3dc2c3eb15f082c85
verstmaster <nagiboter@bk.ru>
2020-02-24 15:26:44 +0300

    Оптимизация партиклов + блок с картинкой и паралакс на ней

--- js/main.js
+++ js/main.js
@@ -4,8 +4,24 @@ $(document).ready(function(){
 
 var custom = {
     banner:{
+        events:function(){
+            var _ = this;
+            $(document).on('scroll',function(){
+                window.pJSDom.forEach(function(item,key){
+                    if($(document).scrollTop()*2 > $('.banner-bg').height()){
+                        item.pJS.particles.move.enable = false
+                    }else if(!item.pJS.particles.move.enable){
+                        item.pJS.particles.move.enable = true;
+                        item.pJS.fn.particlesRefresh()
+                    }
+                })
+                
+            })
+        },
         createParticles:function(){
+            var _ = this;
             particlesJS.load('particles-js', 'libs/particles/particles.json', function() {
+                _.events()
             });
             particlesJS.load('particles-js-bg', 'libs/particles/particles.json', function() {
             });
@@ -14,8 +30,46 @@ var custom = {
             this.createParticles();
         }
     },
+    sliderTools:{
+        createSlider:function(){
+            var mySwiper = new Swiper('.tools-slider', {
+                speed: 500,
+                spaceBetween: 0,
+                slidesPerView:4,
+                slidesPerGroup: 4,
+                loop: true,
+                loopFillGroupWithBlank: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            });
+        },
+        init:function(){
+            this.createSlider()
+        }
+    },
+    fullImg:{
+        doing:function(scrollTop){
+            var top = $('.fullImg-img').offset().top;
+            var paralax =  (scrollTop - top)*0.5 - 50;
+            $('.fullImg-img').css('background-position','center '+paralax+'px');
+        },
+        events:function(){
+            var _ = this;
+            $(document).on('scroll',function(){
+                _.doing($(document).scrollTop());
+            })
+        },
+        init:function(){
+            this.events();
+        },
+    },
     init:function(){
-        this.banner.init();
+        if($('.banner').length>0) this.banner.init();
+        if($('.tools').length>0) this.sliderTools.init();
+        if($('.fullImg-img').length>0) this.fullImg.init();
+        
     }
 }
 
