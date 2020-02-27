de36e7cc8d2e61b399f1bd5f5583600a06f9a9f0
verstmaster <nagiboter@bk.ru>
2020-02-27 08:10:09 +0300

    Адаптив главной до 1024

--- js/main.js
+++ js/main.js
@@ -1,139 +1,193 @@
-$(document).ready(function(){
-    custom.init();
-})
+$(document).ready(function() {
+  custom.init();
+});
 
 var custom = {
-    header:{
-        doing:function(){
-            if($(document).scrollTop()>50){
-                $('.header').addClass('scroll')
-            }else{
-                $('.header').removeClass('scroll')
-            }
-        },
-        init:function(){
-            var _ = this;
-            $(document).on('scroll',function(){
-                _.doing();
-            })
-            _.doing();
-        }
+  header: {
+    doing: function() {
+      if ($(document).scrollTop() > 50) {
+        $(".header").addClass("scroll");
+      } else {
+        $(".header").removeClass("scroll");
+      }
     },
-    banner:{
-        events:function(){
-            var _ = this;
-            $(document).on('scroll',function(){
-                window.pJSDom.forEach(function(item,key){
-                    if($(document).scrollTop() > $('.banner-bg').height()){
-                        item.pJS.particles.move.enable = false
-                    }else if(!item.pJS.particles.move.enable){
-                        item.pJS.particles.move.enable = true;
-                        item.pJS.fn.particlesRefresh()
-                    }
-                })
-                
-            })
-        },
-        createParticles:function(){
-            var _ = this;
-            particlesJS.load('particles-js', 'libs/particles/particles.json', function() {
-                _.events()
-            });
-            particlesJS.load('particles-js-bg', 'libs/particles/particles.json', function() {
-            });
-        },
-        init:function(){
-            this.createParticles();
-        }
-    },
-    sliderTools:{
-        createSlider:function(){
-            var mySwiper = new Swiper('.tools-slider', {
-                speed: 500,
-                spaceBetween: 0,
-                slidesPerView:4,
-                slidesPerGroup: 4,
-                loop: true,
-                loopFillGroupWithBlank: true,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-            });
-        },
-        init:function(){
-            this.createSlider()
-        }
-    },
-    fullImg:{
-        doing:function(scrollTop){
-            var top = $('.fullImg-img').offset().top;
-            var paralax =  (scrollTop - top)*0.5 - 50;
-            $('.fullImg-img').css('background-position','center '+paralax+'px');
-        },
-        events:function(){
-            var _ = this;
-            $(document).on('scroll',function(){
-                _.doing($(document).scrollTop());
-            })
-        },
-        init:function(){
-            this.events();
-        },
-    },
-    gotoUp:{
-        doing:function(scrollTop){
-            if(scrollTop>$(window).height()){
-                $('.goto-up').addClass('show')
-                $('.goto-down').addClass('hide')
-                
-            }else{
-                $('.goto-up').removeClass('show')
-                $('.goto-down').removeClass('hide')
-            }
-        },
-        events:function(){
-            var _ = this;
-            $(document).on('scroll',function(){
-                _.doing($(document).scrollTop());
-            })
-        },
-        init:function(){
-            this.events();
-            this.doing($(document).scrollTop());
-        }
-    },
-    portfolioImgs:{
-        zomm:function(){
-            $(".portfolio-img-layers .layer").fancybox();
-            $(".present-layers .layer-box").fancybox();
-        },
-        scrolled:function(){
-            $(document).on('scroll',function(){
-                var scroll = $(document).scrollTop();
-                var height = $(window).height();
-                $(".portfolio-item").each(function(){
-                    var top = $(this).offset().top;
-                    if(scroll + height/2 > top && scroll>50){
-                        $(this).addClass('showed')
-                    }else{
-                        $(this).removeClass('showed')
-                    }
-                })
-            })
-        },
-        init:function(){
-           this.zomm();
-           this.scrolled();
-        },
-    },
-    init:function(){
-        this.header.init();
-        if($('.banner').length>0) this.banner.init();
-        if($('.tools').length>0) this.sliderTools.init();
-        if($('.fullImg-img').length>0) this.fullImg.init();
-        if($('.goto-up').length>0) this.gotoUp.init();
-        if($('.layer').length>0) this.portfolioImgs.init();
+    init: function() {
+      var _ = this;
+      $(document).on("scroll", function() {
+        _.doing();
+      });
+      _.doing();
     }
-}
-
+  },
+  banner: {
+    events: function() {
+      var _ = this;
+      $(document).on("scroll", function() {
+        window.pJSDom.forEach(function(item, key) {
+          if ($(document).scrollTop() > $(".banner-bg").height()) {
+            item.pJS.particles.move.enable = false;
+          } else if (!item.pJS.particles.move.enable) {
+            item.pJS.particles.move.enable = true;
+            item.pJS.fn.particlesRefresh();
+          }
+        });
+      });
+    },
+    createParticles: function() {
+      var _ = this;
+      particlesJS.load(
+        "particles-js",
+        "libs/particles/particles.json",
+        function() {
+          _.events();
+        }
+      );
+      particlesJS.load(
+        "particles-js-bg",
+        "libs/particles/particles.json",
+        function() {}
+      );
+    },
+    init: function() {
+      this.createParticles();
+    }
+  },
+  sliderTools: {
+    createSlider: function() {
+      var sliderTools = new Swiper(".tools-slider", {
+        speed: 500,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        breakpoints: {
+            900: {
+                slidesPerView: 4,
+                slidesPerGroup: 4,
+            },
+            700: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            500: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+            },
+        }
+      });
+    },
+    init: function() {
+      this.createSlider();
+    }
+  },
+  doingSlider: {
+    createSlider: function() {
+      var doingList = new Swiper(".doing-list", {
+        speed: 500,
+        spaceBetween: 40,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        breakpoints: {
+          1500: {
+            spaceBetween: 60
+          },
+          1400: {
+            slidesPerView: 3,
+            slidesPerGroup: 3
+          },
+          1150: {
+            spaceBetween: 60,
+            slidesPerView: 2,
+            slidesPerGroup: 2
+          },
+          1024: {
+            slidesPerView: 3,
+            slidesPerGroup: 3
+          },
+          700: {
+            slidesPerView: 2,
+            slidesPerGroup: 2
+          }
+        }
+      });
+    },
+    init: function() {
+      this.createSlider();
+    }
+  },
+  fullImg: {
+    doing: function(scrollTop) {
+      var top = $(".fullImg-img").offset().top;
+      var paralax = (scrollTop - top) * 0.5 - 50;
+      $(".fullImg-img").css("background-position", "center " + paralax + "px");
+    },
+    events: function() {
+      var _ = this;
+      $(document).on("scroll", function() {
+        _.doing($(document).scrollTop());
+      });
+    },
+    init: function() {
+      this.events();
+    }
+  },
+  gotoUp: {
+    doing: function(scrollTop) {
+      if (scrollTop > $(window).height()) {
+        $(".goto-up").addClass("show");
+        $(".goto-down").addClass("hide");
+      } else {
+        $(".goto-up").removeClass("show");
+        $(".goto-down").removeClass("hide");
+      }
+    },
+    events: function() {
+      var _ = this;
+      $(document).on("scroll", function() {
+        _.doing($(document).scrollTop());
+      });
+    },
+    init: function() {
+      this.events();
+      this.doing($(document).scrollTop());
+    }
+  },
+  portfolioImgs: {
+    zomm: function() {
+      $(".portfolio-img-layers .layer").fancybox();
+      $(".present-layers .layer-box").fancybox();
+    },
+    scrolled: function() {
+      $(document).on("scroll", function() {
+        var scroll = $(document).scrollTop();
+        var height = $(window).height();
+        $(".portfolio-item").each(function() {
+          var top = $(this).offset().top;
+          if (scroll + height / 2 > top && scroll > 50) {
+            $(this).addClass("showed");
+          } else {
+            $(this).removeClass("showed");
+          }
+        });
+      });
+    },
+    init: function() {
+      this.zomm();
+      this.scrolled();
+    }
+  },
+  init: function() {
+    this.header.init();
+    if ($(".banner").length > 0) this.banner.init();
+    if ($(".tools").length > 0) this.sliderTools.init();
+    if ($(".fullImg-img").length > 0) this.fullImg.init();
+    if ($(".goto-up").length > 0) this.gotoUp.init();
+    if ($(".layer").length > 0) this.portfolioImgs.init();
+    if ($(".doing-list").length > 0) this.doingSlider.init();
+  }
+};
